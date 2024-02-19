9fdad080b650e823f616e0ea2d76c3e4c15ab7ce
kumarvinayak490 <vinayak@ineuron.ai>
2024-02-20 03:58:08 +0530

    updated route for create-user

--- api/auth/url.py
+++ api/auth/url.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('create-user', UserCreateAPIView.as_view(), name='Create User'),
+    path('create-user/', UserCreateAPIView.as_view(), name='Create User'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
